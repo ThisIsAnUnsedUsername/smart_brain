5162bac1608a9d54f8f12c0261d54375f0d361b0
Teik Yee Lim <limteikyee@gmail.com>
2018-10-07 20:17:15 +0800

    end of front end development

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import ImageLinkForm from './component/ImageLinkForm/ImageLinkForm';
 import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
 import './App.css';
+import Signin from './component/Signin/Signin';
+import Register from './component/Register/Register';
 
 const particlesOptions = {
   particles: {
@@ -22,9 +24,31 @@ class App extends Component {
     super();
     this.state = {
       input: '',
-      imageUrl: ''
+      imageUrl: '',
+      box: {},
+      route: 'signin',
+      isSignedIn: false
     };
   }
+
+  calculateFaceLocation = data => {
+    console.log(data);
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height
+    };
+  };
+
+  displayFaceBox = box => {
+    this.setState({ box: box });
+  };
 
   onInputChange = event => {
     this.setState({ input: event.target.value });
@@ -38,34 +62,47 @@ class App extends Component {
       () => {
         app.models
           .predict(Clarifai.FACE_DETECT_MODEL, this.state.imageUrl)
-          .then(
-            function(response) {
-              console.log(
-                response.outputs[0].data.regions[0].region_info.bounding_box
-              );
-            },
-            function(err) {
-              console.log(err);
-              // there was an error
-            }
-          );
+          .then(response => {
+            this.displayFaceBox(this.calculateFaceLocation(response));
+          })
+          .catch(err => console.log(err));
       }
     );
   };
 
+  onRouteChange = route => {
+    if (route === 'signout') {
+      this.setState({ isSignedIn: false });
+    } else if (route === 'home') {
+      this.setState({ isSignedIn: true });
+    }
+    this.setState({ route: route });
+  };
+
   render() {
+    const { isSignedIn, imageUrl, route, box } = this.state;
     return (
       <div className="App">
         <Particles className="particles" params={particlesOptions} />
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm
-          onInputChange={this.onInputChange}
-          onButtonSubmit={this.onButtonSubmit}
+        <Navigation
+          isSignedIn={isSignedIn}
+          onRouteChange={this.onRouteChange}
         />
-
-        <FaceRecognition imageUrl={this.state.imageUrl} />
+        {route === 'home' ? (
+          <div>
+            <Logo />
+            <Rank />
+            <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onButtonSubmit={this.onButtonSubmit}
+            />
+            <FaceRecognition box={box} imageUrl={imageUrl} />
+          </div>
+        ) : route === 'signin' ? (
+          <Signin onRouteChange={this.onRouteChange} />
+        ) : (
+          <Register onRouteChange={this.onRouteChange} />
+        )}
       </div>
     );
   }
--- src/component/Navigation/Navigation.js
+++ src/component/Navigation/Navigation.js
@@ -1,12 +1,38 @@
 import React from 'react';
 
-const Navigation = () => {
-  return (
-    <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
-      {/*justify-content need to written as camel case in JSX*/}
-      <p className="f3 link dim black underline pa3 pointer">Sign Out</p>
-    </nav>
-  );
+const Navigation = ({ onRouteChange, isSignedIn }) => {
+  if (isSignedIn) {
+    return (
+      <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
+        {/*justify-content need to written as camel case in JSX*/}
+        {/*wrap in function or else onRouteChange run when Navigation get rendered*/}
+        {/*cannot comment in tag*/}
+        <p
+          onClick={() => onRouteChange('signout')}
+          className="f3 link dim black underline pa3 pointer"
+        >
+          Sign Out
+        </p>
+      </nav>
+    );
+  } else {
+    return (
+      <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
+        <p
+          onClick={() => onRouteChange('signin')}
+          className="f3 link dim black underline pa3 pointer"
+        >
+          Sign In
+        </p>
+        <p
+          onClick={() => onRouteChange('register')}
+          className="f3 link dim black underline pa3 pointer"
+        >
+          Register
+        </p>
+      </nav>
+    );
+  }
 };
 
 export default Navigation;
